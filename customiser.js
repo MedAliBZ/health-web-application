ab5cc5a81ff759aaad7f9d198f5915c1ae931650
DaliHOST <mohamedali.bouzaiene@esprit.tn>
2020-07-15 22:24:01 +0100

    started on customiser

---
+++ customiser.js
@@ -0,0 +1,12 @@
+let nav=document.querySelector("#test")
+let k
+
+
+nav.addEventListener("click",function ch(){
+
+    k = "red";
+    document.body.style.backgroundColor=k
+})
+console.log(nav)
+
+document.body.style.backgroundColor=k
