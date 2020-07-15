3dba36f27583c155d8b245401d9dabee9aa31ad0
DaliHOST <mohamedali.bouzaiene@esprit.tn>
2020-07-15 18:01:43 +0100

    advanced in calories

--- calories.js
+++ calories.js
@@ -1,4 +1,16 @@
-fetch("https://fitness-calculator.p.rapidapi.com/dailycalory?heigth=180&age=25&gender=male&weigth=70", {
+let age=document.getElementById("age")
+let height=document.getElementById("height")
+let weight=document.getElementById("weight")
+let gender=document.getElementById("gender")
+let goal=document.getElementById("goal")
+
+let calculate = document.getElementById("calculate")
+let k=goal.value
+console.log(k)
+
+
+calculate.addEventListener("click",function result(){
+	fetch(`https://fitness-calculator.p.rapidapi.com/dailycalory?heigth=${height.value}&age=${age.value}&gender=${gender.value}&weigth=${weight.value}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
@@ -7,7 +19,8 @@ fetch("https://fitness-calculator.p.rapidapi.com/dailycalory?heigth=180&age=25&g
 })
 .then(response => {
 	return response.json()
-}).then(data=>console.log(data))
+}).then(data=>console.log(data.data.goals["maintain weight"]))
 .catch(err => {
 	console.log(err);
 });
+})
